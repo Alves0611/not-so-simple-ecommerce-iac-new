87058f297d994f11b72d1a9347d33a9c3a7ff8d9
“Gabriel <g.alves0611@gmail.com>
2025-08-27 17:27:04 -0300

    feat: enable versioning on Terraform state S3 bucket

--- terraform/00-remote-state/s3.tf
+++ terraform/00-remote-state/s3.tf
@@ -4,3 +4,11 @@ resource "aws_s3_bucket" "this" {
   bucket        = "tfstate-${data.aws_caller_identity.current.account_id}"
   force_destroy = true
 }
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
