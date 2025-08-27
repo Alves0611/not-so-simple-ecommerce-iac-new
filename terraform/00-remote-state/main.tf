5def7a16487f2f064e78ef84dfe2b6030110efb9
“Gabriel <g.alves0611@gmail.com>
2025-08-27 17:24:56 -0300

    feat: apply default tags to AWS resources via provider

--- terraform/00-remote-state/main.tf
+++ terraform/00-remote-state/main.tf
@@ -1,7 +1,12 @@
 provider "aws" {
   region = var.region
+
   assume_role {
     role_arn    = var.aws.role_arn
     external_id = var.aws.external_id
+  }
+
+  default_tags {
+    tags = var.tags
   }
 }
