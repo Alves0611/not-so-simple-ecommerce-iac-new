c38280b46a4db0f8763483f4582fda6d575aa52c
“Gabriel <g.alves0611@gmail.com>
2025-08-27 17:44:57 -0300

    fix: use correct account ID in assume role ARN

--- terraform/00-remote-state/variables.tf
+++ terraform/00-remote-state/variables.tf
@@ -10,7 +10,7 @@ variable "aws" {
   })
 
   default = {
-    role_arn    = "arn:aws:iam::891377404175:role/terraform-role"
+    role_arn    = "arn:aws:iam::444065722670:role/terraform-role"
     external_id = "679e2de7-7db8-4114-af04-6620fdae82f9"
   }
 }
