d03a85bf852d27403626782abf86cf2b7624e240
Alves0611 <g.alves0611@gmail.com>
2025-08-28 19:46:26 -0300

    feat: define region, aws role, and default tags variables

---
+++ terraform/01-networking/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "aws" {
+  type = object({
+    role_arn    = string
+    external_id = string
+  })
+
+  default = {
+    role_arn    = "arn:aws:iam::444065722670:role/terraform-role"
+    external_id = "679e2de7-7db8-4114-af04-6620fdae82f9"
+  }
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Project   = "nsse"
+    Managedby = "Terraform"
+  }
+}
