24280a46d96523e421403d85b62f6e16af76b588
“Gabriel <g.alves0611@gmail.com>
2025-08-29 16:34:13 -0300

    feat: create public route table with 0.0.0.0/0 via internet gateway

---
+++ terraform/01-networking/vpc.public-route-tables.tf
@@ -0,0 +1,11 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = { Name = "${var.vpc_resources.vpc}-${var.vpc_resources.public_route_table_name}" }
+}
+
