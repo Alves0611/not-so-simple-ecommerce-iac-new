48dfd369bef8836d06cca709b43e2d4f9b07cfed
Alves0611 <g.alves0611@gmail.com>
2025-09-07 16:26:00 -0300

    feat: add private subnets data source

---
+++ terraform/02-server/data.vpc.private-subnets.tf
@@ -0,0 +1,11 @@
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.this.id]
+  }
+
+  filter {
+    name   = "map-public-ip-on-launch"
+    values = [false]
+  }
+}
